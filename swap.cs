dbff9c17b6798b1255ff7543f1e76e822c55dd13
bozniac <nino.bosnic@yahoo.com>
2017-09-15 13:49:56 +0200

    Add a swap function

--- swap.cs
+++ swap.cs
@@ -13,12 +13,17 @@ namespace Test
             Console.WriteLine($"y is {y}");
 
             // swapping...
-            int temp = x;
-            x = y;
-            y = temp;
+            Swap(ref x, ref y);
 
             Console.WriteLine($"x is now {x}");
             Console.WriteLine($"y is now {y}");
+        }
+
+        public static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
         }
     }
 }
