f9b8030280a62949c8263369aa5da2896eccda1e
bozniac <nino.bosnic@yahoo.com>
2017-09-15 11:49:37 +0200

    Initial commit

---
+++ swap.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 28;
+            int y = 50;
+
+            Console.WriteLine($"x is {x}");
+            Console.WriteLine($"y is {y}");
+
+            // swapping...
+
+            Console.WriteLine($"x is now {x}");
+            Console.WriteLine($"y is now {y}");
+        }
+    }
+}
